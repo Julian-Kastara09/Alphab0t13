271a5d63f2880bf4352846fa8ed88b41fe9f6231
Julian-Kastara09 <97008779+Julian-Kastara09@users.noreply.github.com>
2022-01-03 11:55:04 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,10 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+_*1.📍Mitsuri-Botz Official📍*_
+_*Link : https://chat.whatsapp.com/Ewz6gvAGcmd8gfrRfpBke7*_
+_*2.📍Mitsuri-Botz Official📍*_
+_*Link : https://chat.whatsapp.com/Czk3Jr8IvZw4klzSUEvDZQ*_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
